ba0a0bae6a9bbbfc9b73c741120e50aa582ffd48
Odebisi idowu solomon <odebisiidowu09@gmail.com>
2025-06-13 12:02:33 +0100

    made changes

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -2,25 +2,25 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script.js";
 
-import "../../public/css/settings.css"
-import "../../public/css/css_1.css"
-import "../../public/css/css.css"
-import "../../public//css/reset.css"
-import "../../public/css/superfish.css"
-import "../../public/css/prettyPhoto.css"
-import "../../public/css/jquery.qtip.css"
-import "../../public/css/style_4.css"
-import "../../public/css/animations.css"
-import "../../public/css/responsive.css"
-import "../../public/css/odometer-theme-default.css"
-import "../../public/css/style_3.css"
-import "../../public/css/style.css"
-import "../../public/css/style_2.css"
-import "../../public/css/style_1.css"
-import "../../public/css/style_selector.css"
+// import "../../public/css/settings.css"
+// import "../../public/css/css_1.css"
+// import "../../public/css/css.css"
+// import "../../public//css/reset.css"
+// import "../../public/css/superfish.css"
+// import "../../public/css/prettyPhoto.css"
+// import "../../public/css/jquery.qtip.css"
+// import "../../public/css/style_4.css"
+// import "../../public/css/animations.css"
+// import "../../public/css/responsive.css"
+// import "../../public/css/odometer-theme-default.css"
+// import "../../public/css/style_3.css"
+// import "../../public/css/style.css"
+// import "../../public/css/style_2.css"
+// import "../../public/css/style_1.css"
+// import "../../public/css/style_selector.css"
 
-import Preloader from "../_components/UI/preloader";
-import SearchAbsoluteContainer from "../_components/UI/SearchAbsoluteContainer";
+// import Preloader from "../_components/UI/preloader";
+// import SearchAbsoluteContainer from "../_components/UI/SearchAbsoluteContainer";
 
 
 const geistSans = Geist({
